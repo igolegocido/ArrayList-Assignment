8228c4d8a3ab181d1b3b790c08648ee6d6d97c93
igolegocido <52680690+igolegocido@users.noreply.github.com>
2021-12-02 09:05:45 -0800

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+class Question1 {
+    
+    public static ArrayList<String> reverse( ArrayList<String> myList ) {
+        //This method takes an ArrayList<String> as a parameter and returns
+        //a new ArrayList<String> in which the elements are stored in reverse
+        //order.  The original list should remain unchanged.
+        ArrayList<String> ans = new ArrayList<String>();
+        for(String a : myList){
+            ans.add(0,a);
+        }
+        return ans;
+        
+    }
+    
+    public static void main (String[] args) {
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("Why");
+        words.add("do");
+        words.add("we");
+        words.add("fall");
+        words.add("Bruce?");
+        words.add("So");
+        words.add("we");
+        words.add("can");
+        words.add("learn");
+        words.add("to");
+        words.add("pick");
+        words.add("ourselves");
+        words.add("up");
+        System.out.println("List forwards reads: " + words);
+        System.out.println("List backward reads: " + reverse(words));
+    }
+    
+    
+}
